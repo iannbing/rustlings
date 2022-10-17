3c7862bb26823c01ef6447f2091d1903fed2f891
Huang-Ming Chang <huang@orchest.io>
2022-10-17 14:15:48 +0200

    move_semantics2

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,10 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn main() {
     let vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(&vec0);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -17,8 +15,8 @@ fn main() {
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.to_vec();
 
     vec.push(22);
     vec.push(44);
@@ -26,3 +24,31 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
     vec
 }
+
+/* hint
+3. Make `fill_vec` *mutably* borrow a reference to its argument (which will need to be
+   mutable), modify it directly, then not return anything. Then you can get rid
+   of `vec1` entirely -- note that this will change what gets printed by the
+   first `println!`
+*/
+
+// Approach 3
+
+// fn main() {
+//     let mut vec0: Vec<i32> = Vec::new();
+
+//     fill_vec(&mut vec0);
+
+//     // Do not change the following line!
+//     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+//     vec0.push(88);
+
+//     println!("{} has length {} content `{:?}`", "vec1", vec0.len(), vec0);
+// }
+
+// fn fill_vec(vec: &mut Vec<i32>) {
+//     vec.push(22);
+//     vec.push(44);
+//     vec.push(66);
+// }
