b6ece8f9299ef2f3203e8a1cf4cc3c21bf0338a5
Huang-Ming Chang <huang@orchest.io>
2022-10-19 11:23:49 +0200

    hashmaps2

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -11,8 +11,6 @@
 //
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 #[derive(Hash, PartialEq, Eq)]
@@ -37,6 +35,10 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        let is_allowed = basket.get(&fruit) == None;
+        if is_allowed {
+            basket.insert(fruit, 1);
+        }
     }
 }
 
